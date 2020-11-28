efbb6f2287f675ffbfd40d3a60cd18a8a41c30b9
Hames1001 <jamesrazo12@gmail.com>
2020-11-28 14:49:06 -0800

    workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hames10/ros_workspaces/106a-final-proj/src
+# Build directory: /home/hames10/ros_workspaces/106a-final-proj/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
